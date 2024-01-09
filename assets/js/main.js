f1be8c7471778f9fc03294ef604d059dc35a4f95
hollytech30 <hollytech30@gmail.com>
2024-01-09 18:44:49 +0300

    by tony

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,22 @@
   }
 
   /**
+   * Hero type effect
+   */
+  // let typed = select('.typed')
+  // if (typed) {
+  //   let typed_strings = typed.getAttribute('data-typed-items')
+  //   typed_strings = typed_strings.split(',')
+  //   new Typed('.typed', {
+  //     strings: typed_strings,
+  //     loop: true,
+  //     typeSpeed: 100,
+  //     backSpeed: 50,
+  //     backDelay: 2000
+  //   });
+  // }
+
+  /**
    * Navbar links active state on scroll
    */
   let navbarlinks = select('#navbar .scrollto', true)
@@ -219,5 +235,5 @@
    * Initiate Pure Counter 
    */
   new PureCounter();
-
+  
 })()
