935a4bc2c38b7067416d25a26824c4abe6a9a884
hollytech30 <130698974+hollytech30@users.noreply.github.com>
2024-01-22 20:08:22 +0300

    final

--- controllers/dbconn.php
+++ controllers/dbconn.php
@@ -1,14 +1,14 @@
 <?php
 
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPassword = '';
-$dbName = 'raziamunira';
+// $dbHost = 'localhost';
+// $dbUser = 'root';
+// $dbPassword = '';
+// $dbName = 'raziamunira';
 
-// $dbHost = 'your_db_host';
-// $dbUser = 'your_db_user';
-// $dbPassword = 'your_db_password';
-// $dbName = 'your_db_name';
+$dbHost = 'sql213.infinityfree.com';
+$dbUser = 'if0_35799655';
+$dbPassword = 'gquowvE8VE2C';
+$dbName = 'if0_35799655_raziamunira';
 
 // Establish a database connection
 $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
